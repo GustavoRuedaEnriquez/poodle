2d80a5dd41b89eddcea31e27536119b0224cce80
GustavoRuedaEnriquez <gusgus2208@gmail.com>
2019-12-02 19:39:41 -0600

    Meetings controller updated.

--- api/controllers/meeting.js
+++ api/controllers/meeting.js
@@ -2,27 +2,76 @@
 
 let Meeting = require('../models/meeting');
 
-function addProposal(req, res){
+function addMeeting(req, res){
     let meeting = new Meeting();
     let params = req.body;
 
-    if(params.date == undefined){
-        res.status(400).send({mesage:'Bad request error. The date field is missing'});
+    if(params.name == undefined){
+        res.status(400).send({mesage:'Bad request error. The name field is missing'});
+    }
+    if(params.description == undefined){
+        res.status(400).send({mesage:'Bad request error. The description field is missing'});
+    }
+    if(params.importance == undefined){
+        res.status(400).send({mesage:'Bad request error. The importance field is missing'});
+    }
+    if(params.organizer == undefined){
+        res.status(400).send({mesage:'Bad request error. The organizer field is missing'});
     }
 
-    proposal.date = params.date;
-    proposal.votes = (params.votes == undefined) ? 0 : params.votes;
-    proposal.voters = (params.voters == undefined) ? [] : params.voters;
+    meeting.name = params.name;
+    meeting.description = params.description;
+    meeting.participants_number = (params.participants_number == undefined) ? 0 : params.participants_number;
+    meeting.participants = (params.participants == undefined) ? [] : params.participants;
+    meeting.date = params.date;
+    meeting.importance = params.importance;
+    meeting.organizer = params.organizer;
+    meeting.schedule_proposal = (params.schedule_proposal == undefined) ? [] : params.schedule_proposal;
 
-    proposal.save((err, storedProposal) => {
+    meeting.save((err, storedMeeting) => {
         if(err){
             res.status(500).send({mesage:'Server error.'});
         } else {
             if(!storedUser){
                 res.status(404).send({message:'Error while saving.'});
             } else {
-                res.status(201).send({message:'Proposal stored', proposal : storedProposal});
+                res.status(201).send({message:'Meeting stored', meeting : storedMeeting});
+            }
+        }
+    });   
+}
+
+function getMeetings(req,res){
+    Meeting.find({},(err, meetings) => {
+        if(err){
+            res.status(500).send({message: 'Server error.'});
+        }else{
+            if(Object.entries(proposals).length === 0){
+                res.status(404).send({message: 'No meetings found.'});
+            }else{
+                res.status(200).send({message:'Meetings obtained', results : meetings});
             }
         }
     });
+}
+
+function getMeetingById(req,res){
+    let meetingId = req.params.id;
+    Meeting.findById(meetingId,(err,proposal) => {
+        if(err){
+            res.status(500).send({message: 'Server error.'});
+        }else{
+            if(Object.entries(proposal).length === 0){
+                res.status(404).send({message: 'Meeting not found.'});
+            }else{
+                res.status(200).send({message:'Meeting obtained', result : proposal});
+            }
+        }
+    });
+}
+
+module.exports = {
+    addMeeting,
+    getMeetings,
+    getMeetingById
 }
